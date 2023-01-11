f521768c61a15420dba97f05389792973fc24bb2
gianniskts <johnkotsias@hotmail.gr>
2023-01-12 01:01:17 +0200

    defs

--- kernel/defs.h
+++ kernel/defs.h
@@ -24,6 +24,7 @@ void            consputc(int);
 
 // exec.c
 int             exec(char*, char**);
+void            vmprint(pagetable_t);
 
 // file.c
 struct file*    filealloc(void);
@@ -173,7 +174,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-void            vmprint(pagetable_t);
+uint64          lazyAllocation(struct proc*, uint64);
 
 // plic.c
 void            plicinit(void);
